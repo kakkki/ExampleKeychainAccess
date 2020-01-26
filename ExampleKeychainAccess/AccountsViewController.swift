ca07672e61d17be9179d215324e570ca5ed65374
kakehi-binc <kakehi@binc.jp>
2020-01-26 15:30:23 +0900

    add AccountsViewController as the entry point

---
+++ ExampleKeychainAccess/AccountsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AccountsViewController.swift
+//  ExampleKeychainAccess
+//
+//  Created by Atsuki Kakehi on 2020/01/26.
+//  Copyright © 2020 Atsuki Kakehi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AccountsViewControlelr: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("debug0000 : AccountsViewControlelr.numberOfRowsInSection")
+        return 0
+    }
+    
+}
