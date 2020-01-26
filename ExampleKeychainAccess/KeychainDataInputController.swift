b01919a9ae4033bc60091ecea8fb2741f385e620
kakehi-binc <kakehi@binc.jp>
2020-01-26 16:36:10 +0900

    Keychainにsaveする処理を実装

--- ExampleKeychainAccess/KeychainDataInputController.swift
+++ ExampleKeychainAccess/KeychainDataInputController.swift
@@ -8,10 +8,17 @@
 
 import Foundation
 import UIKit
+import KeychainAccess
 
 class KeychainDataInputController: UITableViewController {
     
     @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var serviceField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +29,36 @@ class KeychainDataInputController: UITableViewController {
         print("debug0000 : cancel")
     }
     
+    
+    @IBAction func saveAction(_ sender: UIBarButtonItem) {
+        let keychain: Keychain
+        if let service = serviceField.text, !service.isEmpty {
+            keychain = Keychain(service: service)
+        } else {
+            keychain = Keychain()
+        }
+        keychain[usernameField.text!] = passwordField.text
+
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+        switch (usernameField.text, passwordField.text) {
+        case let (username?, password?):
+            saveButton.isEnabled = !username.isEmpty && !password.isEmpty
+            print("debug0000 : case1")
+        case (_?, nil):
+            saveButton.isEnabled = false
+            print("debug0000 : case2")
+        case (nil, _?):
+            saveButton.isEnabled = false
+            print("debug0000 : case3")
+        case (nil, nil):
+            saveButton.isEnabled = false
+            print("debug0000 : case4")
+        }
+
+    }
 }
